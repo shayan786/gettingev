a70de7c29afeb0e0a09803fa3db6093db84fd22c
shayan786 <shayandhanani@gmail.com>
2021-03-17 22:56:04 -0500

    details panel

--- src/components/DetailsPanel/DetailsPanel.js
+++ src/components/DetailsPanel/DetailsPanel.js
@@ -6,6 +6,10 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Carousel from 'react-material-ui-carousel';
 import { api } from '../../utils/api.js';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Divider from '@material-ui/core/Divider';
 
 class DetailsPanel extends Component {
 	_renderHeader (title, handleClose) {
@@ -19,15 +23,191 @@ class DetailsPanel extends Component {
 		)
 	}
 
-	_renderCarousel(car) {
+	_renderCarousel (car) {
 		return (
-			<Carousel>
+			<Carousel className={s.carousel}>
       	{
-      		car.images.map((image, index) => <img className={s.image} key={index} src={`${api.url}${image.formats.medium.url}`} />)
+      		car.images.map((image, index) => <img className={s.image} key={index} src={image.formats.medium ? `${api.url}${image.formats.medium.url}` : `${api.url}${image.formats.thumbnail.url}`} />)
       	} 	     
       </Carousel>
 		)
 	}
+
+  _renderOverall (car) {
+    const overallMap = [
+      {
+        label: "Range (mi)",
+        value: car.range
+      },
+      {
+        label: "Battery (kw)",
+        value: car.battery
+      },
+      {
+        label: "0-60mph (s)",
+        value: car.acceleration
+      },
+      {
+        label: "Top Speed (mph)",
+        value: car.top_speed
+      }
+    ];
+
+    return (
+      <List
+        className={s.overall}
+        subheader={
+          <ListSubheader disableSticky={true}>
+            Overall
+          </ListSubheader>
+        }>
+        {
+          overallMap.map((prop, i) => 
+            <ListItem className={s.listItem} key={i}>
+              <div>
+                {prop.label}
+              </div>
+              <div>
+                {prop.value}
+              </div>
+            </ListItem>
+          )
+        }
+      </List>
+    )
+  }
+
+  _renderBattery (car) {
+    const overallMap = [
+      {
+        label: "Range (mi)",
+        value: car.range
+      },
+      {
+        label: "Battery (kw)",
+        value: car.battery
+      },
+      {
+        label: "0-60mph (s)",
+        value: car.acceleration
+      },
+      {
+        label: "Top Speed (mph)",
+        value: car.top_speed
+      }
+    ];
+
+    return (
+      <List
+        className={s.overall}
+        subheader={
+          <ListSubheader disableSticky={true}>
+            Battery and Charging
+          </ListSubheader>
+        }>
+        {
+          overallMap.map((prop, i) => 
+            <ListItem className={s.listItem} key={i}>
+              <div>
+                {prop.label}
+              </div>
+              <div>
+                {prop.value}
+              </div>
+            </ListItem>
+          )
+        }
+      </List>
+    )
+  }
+
+  _renderPower (car) {
+    const overallMap = [
+      {
+        label: "Range (mi)",
+        value: car.range
+      },
+      {
+        label: "Battery (kw)",
+        value: car.battery
+      },
+      {
+        label: "0-60mph (s)",
+        value: car.acceleration
+      },
+      {
+        label: "Top Speed (mph)",
+        value: car.top_speed
+      }
+    ];
+
+    return (
+      <List
+        className={s.overall}
+        subheader={
+          <ListSubheader disableSticky={true}>
+            Powertrain
+          </ListSubheader>
+        }>
+        {
+          overallMap.map((prop, i) => 
+            <ListItem className={s.listItem} key={i}>
+              <div>
+                {prop.label}
+              </div>
+              <div>
+                {prop.value}
+              </div>
+            </ListItem>
+          )
+        }
+      </List>
+    )
+  }
+
+  _renderBody (car) {
+    const overallMap = [
+      {
+        label: "Range (mi)",
+        value: car.range
+      },
+      {
+        label: "Battery (kw)",
+        value: car.battery
+      },
+      {
+        label: "0-60mph (s)",
+        value: car.acceleration
+      },
+      {
+        label: "Top Speed (mph)",
+        value: car.top_speed
+      }
+    ];
+
+    return (
+      <List
+        className={s.overall}
+        subheader={
+          <ListSubheader disableSticky={true}>
+            Body and Chassis
+          </ListSubheader>
+        }>
+        {
+          overallMap.map((prop, i) => 
+            <ListItem className={s.listItem} key={i}>
+              <div>
+                {prop.label}
+              </div>
+              <div>
+                {prop.value}
+              </div>
+            </ListItem>
+          )
+        }
+      </List>
+    )
+  }
 
   render() {
   	const { open, car, handleClose } = this.props;
@@ -37,6 +217,13 @@ class DetailsPanel extends Component {
     		{ this._renderHeader(`${car.year} ${car.manufacturer} ${car.model} - ${car.trim}`, handleClose) }
     		<div className={s.body}>
     			{ this._renderCarousel(car) }
+          { this._renderOverall(car) }
+          <Divider />
+          { this._renderBattery(car) }
+          <Divider />
+          { this._renderPower(car) }
+          <Divider />
+          { this._renderBody(car) }
     		</div>
     	</Paper>
     ) : null
